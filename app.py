6f0156a5838a598dc542fe0abb0f012a49cb0a0d
Ubuntu <ubuntu@ip-10-0-14-173.ap-south-1.compute.internal>
2025-02-18 21:46:01 +0000

    adding python microservice

---
+++ app.py
@@ -0,0 +1,24 @@
+from fastapi import FastAPI
+import socket
+import os
+
+app = FastAPI()
+
+@app.get("/info")
+def get_info():
+    return {
+        "message": "Hello from FastAPI!",
+        "pod_name": socket.gethostname(),
+        "node_name": os.getenv("NODE_NAME", "Unknown"),
+        "namespace": os.getenv("POD_NAMESPACE", "default"),
+        "image_version": os.getenv("IMAGE_VERSION", "1.0.0")
+    }
+
+@app.get("/")
+def root():
+    return {"message": "Welcome to the FastAPI microservice!"}
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8080)
+
