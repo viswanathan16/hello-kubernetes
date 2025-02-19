26fd14d65a7060c4356b0a2ea457490127b8cb61
viswanathan16 <viswanathanram1607@gmail.com>
2025-02-19 22:48:21 +0000

    changes in app.py

--- app.py
+++ app.py
@@ -6,20 +6,22 @@ app = FastAPI()
 
 @app.get("/info")
 def get_info():
+    pod_name = socket.gethostname()
+    node_name = os.getenv("NODE_NAME", "Unknown")
+    namespace = os.getenv("POD_NAMESPACE", "default")
+    image_version = os.getenv("IMAGE_VERSION", "1.0.0")
+
     return {
-        "message": "Hello from FastAPI!",
-        "pod_name": socket.gethostname(),
-        "node_name": os.getenv("NODE_NAME", "Unknown"),
-        "namespace": os.getenv("POD_NAMESPACE", "default"),
-        "image_version": os.getenv("IMAGE_VERSION", "1.0.0")
-    }
+        "message": f"Hi! This is {pod_name} running in {namespace} namespace.",
+        "pod_name": pod_name,
+        "node_name": node_name,
+        "namespace": namespace,
+        "image_version": image_version,
+        "status": "Service is running smoothly!"
 
 @app.get("/")
 def root():
-    return { 
-             "message": "Welcome to the FastAPI microservice!"
-             "info": f"Hi! This is {pod_name} pod responding to your request."
-            }
+    return {"message": "Welcome to the FastAPI microservice!"}
 
 if __name__ == "__main__":
     import uvicorn
